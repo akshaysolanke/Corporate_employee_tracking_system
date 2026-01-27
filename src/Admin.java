9c4ed8f246931428af6c315f73ebb6a992afd8ab
akshaysolanke <akshayashoksolanke@gmail.com>
2026-01-28 00:13:25 +0530

    employee add feature added

--- src/Admin.java
+++ src/Admin.java
@@ -3,6 +3,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 class connection extends Admin{
+   
     void con() throws ClassNotFoundException, SQLException
     {
             //class loaded
@@ -23,14 +24,16 @@ class connection extends Admin{
     }
 }
 
+
 public class Admin {
 
     Connection cn;
     Statement st;
+    Scanner sc;
     void access() throws SQLException,ClassNotFoundException
     {
         
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         String query = "select username, passwords from admin";
         ResultSet rs = st.executeQuery(query);
  
@@ -46,6 +49,7 @@ public class Admin {
             if(uname.equals(aname) && upass.equals(apass))
             {
                 System.out.println("login successful");
+                operations();
             }
             else
             {
@@ -53,6 +57,102 @@ public class Admin {
             }
         }
         
+    }
+    void operations()throws SQLException
+    {   
+      int ch;
+        do { 
+            System.out.println("1.Add employee");
+            System.out.println("2.delete employee");
+            System.out.println("3.update employee");//dept,proj,atten
+            System.out.println("4.Assign project to Employee");
+            System.out.println("5.Assign Department to Employee");
+            System.out.println("6.View Reports");
+            System.out.println("7.logout");
+
+            System.out.println("Enter your choice");
+            ch=sc.nextInt();
+            switch(ch)
+            {
+                case 1 :addemp();
+                        break;
+                case 2: delemp();
+                        break;
+                case 3: updateemp();
+                        break;
+                case 4: assignproj();
+                        break;
+                case 5: assigndept();
+                        break;
+                case 6: viewrepo();
+                        break;
+                case 7:logout();
+                        break;
+                default:System.out.println("Enter valid choice");                                                   
+            }
+        } while (ch!=7);
+    }
+    void addemp() throws SQLException
+    {   
+        String insert = "insert into employee values(?,?,?,?,?,?)";
+        PreparedStatement ps = cn.prepareStatement(insert);
+        System.out.println("Enter no of employees to add");
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Enter "+(i+1)+" Employee details: ");
+            System.out.println("Enter Employee ID: ");
+            int id = sc.nextInt();
+
+            System.out.println("Enter Employee Name: ");
+            String name = sc.next();
+
+            System.out.println("Enter Employee Email: ");
+            String email = sc.next();
+
+            System.out.println("Enter Employee Role: ");
+            String  role = sc.next();
+
+            System.out.println("Enter Employee Project ID: ");
+            int pid = sc.nextInt();
+
+            System.out.println("Enter Employee Department ID: ");
+            int did = sc.nextInt();
+
+            ps.setInt(1, id);
+            ps.setString(2, name);
+            ps.setString(3, email);
+            ps.setString(4, role);
+            ps.setInt(5, pid);
+            ps.setInt(6, did);
+            ps.executeUpdate();
+
+        }
+        System.out.println("Employee Added..!\n\n");
+    }
+    void delemp()
+    {
+        System.out.println("delete");
+    }
+    void updateemp()
+    {
+        System.out.println("update");
+    }
+    void assignproj()
+    {
+        System.out.println("proje");
+    }
+    void assigndept()
+    {
+        System.out.println("dept");
+    }
+    void viewrepo()
+    {
+        System.out.println("report");
+    }
+    void logout()
+    {
+        System.out.println("logout successful..!");
     }
     public static void main(String[] args) throws Exception,ClassNotFoundException {
         connection e= new connection();
